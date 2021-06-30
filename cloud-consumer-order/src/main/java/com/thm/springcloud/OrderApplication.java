0aeb8f68c22b1373745ba95a5928a1163f07707d
hmtang <836039001@qq.com>
2021-06-30 20:20:40 +0800

    断路器代码集成提交

--- cloud-consumer-order/src/main/java/com/thm/springcloud/OrderApplication.java
+++ cloud-consumer-order/src/main/java/com/thm/springcloud/OrderApplication.java
@@ -3,10 +3,12 @@ package com.thm.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class OrderApplication {
 
     public static void main(String[] args) {
--- cloud-consumer-order/src/main/java/com/thm/springcloud/controller/OrderController.java
+++ cloud-consumer-order/src/main/java/com/thm/springcloud/controller/OrderController.java
@@ -1,10 +1,13 @@
 package com.thm.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.thm.springcloud.base.CommonResult;
 import com.thm.springcloud.entity.Order;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/order")
@@ -13,10 +16,16 @@ public class OrderController {
     @Value("${server.port}")
     private String port;
 
+    @HystrixCommand(fallbackMethod = "getOrderFallBack")
     @RequestMapping("/get")
     public CommonResult getOrder(){
         Order order = new Order();
+        int i = 1/0;
         return CommonResult.success(port,order);
+    }
+
+    public CommonResult getOrderFallBack(){
+        return CommonResult.success("error !!!",new Order());
     }
 
 }
